59c0d1d86951988508eb2e1560359fe3f6d5db35
Dan Griffin <dcompgriff@gmail.com>
2016-10-31 16:41:39 -0500

    Implemented code for q2.

--- HW7.py
+++ HW7.py
@@ -1,7 +1,7 @@
 import numpy as np
 import time
 import random as rand
-import cvxpy as cvxpy
+import cvxpy as cvx
 
 
 
@@ -25,12 +25,38 @@ def q1_partb():
 
 
 
+def q2():
+    A = np.array([[1, 0],
+         [2, 1],
+         [1, 3]])
+    b = np.array([[4],
+                  [5],
+                  [7]])
+    lambd = 0.01
+
+    #Normal Equation Solution.
+    x1 = np.linalg.inv(A.T.dot(A) + lambd).dot(A.T).dot(b)
+    print('Normal Equation Solution: ')
+    print(str(x1))
+
+    #Convex optimization solution.
+    x2 = cvx.Variable(2)
+    lambd2 = cvx.Parameter(sign="positive")
+    lambd2.value = 0.01
+    objective = cvx.Minimize(cvx.sum_entries(cvx.square(A*x2 - b)) + (lambd2*cvx.sum_entries(cvx.square(x2))))
+    prob = cvx.Problem(objective)
+    print('\nCVX Optimal value: %f' %(prob.solve()))
+    print('CVX Optimal x: ')
+    print(str(x2.value))
+
+
+
 
 
 def main():
     print('Running main...')
-    q1_partb()
-
+    #q1_partb()
+    q2()
 
 
 
